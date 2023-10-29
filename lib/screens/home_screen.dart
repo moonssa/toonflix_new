5d669b818e1c54b072323d35bd74ea1962ac4b99
moonk <mkjoehsb@gmail.com>
2023-10-30 00:27:18 +0900

    future builder

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,45 +2,32 @@ import 'package:flutter/material.dart';
 import 'package:toonflix_new/models/webtoon_model.dart';
 import 'package:toonflix_new/services/api_services.dart';
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+class HomeScreen extends StatelessWidget {
+  HomeScreen({super.key});
 
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  List<WebtoonModel> webtoons = [];
-  bool isLoading = true;
-
-  void waitForWebToons() async {
-    webtoons = await ApiService.getTodaysToons();
-    isLoading = false;
-    setState(() {});
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    waitForWebToons();
-  }
-
+  Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
   @override
   Widget build(BuildContext context) {
-    print(webtoons);
-    print(isLoading);
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: const Text("오늘의 웹툰",
-            style: TextStyle(
-              fontSize: 22,
-              fontWeight: FontWeight.w600,
-            )),
         backgroundColor: Colors.white,
-        foregroundColor: Colors.green,
-        elevation: 1,
-      ),
-    );
+        appBar: AppBar(
+          title: const Text("오늘의 웹툰",
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.w600,
+              )),
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.green,
+          elevation: 1,
+        ),
+        body: FutureBuilder(
+          future: webtoons,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return const Text("There is Data!!");
+            }
+            return const Text("Loading.....");
+          },
+        ));
   }
 }
